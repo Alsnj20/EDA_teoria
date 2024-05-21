cfcc4898e5309e3baed6b14a9ae5ac8773a8bc1e
Alsnj20 <marieljara656@gmail.com>
2024-05-21 11:49:42 -0500

    Clase Node

---
+++ List/Node.java
@@ -0,0 +1,30 @@
+package List;
+
+public class Node<T>{
+  private T data;
+  private Node<T> next;
+  public Node(T data){
+    this(data, null);
+  }
+  public Node(T data, Node<T> next){
+    this.data = data;
+    this.next = next;
+  }
+  /* Setter and Getter */
+  public void setData(T data){
+    this.data = data;
+  }
+  public void setNext(Node<T> next){
+    this.next = next;
+  }
+  public T getData(){
+    return data;
+  }
+  public Node<T> getNext(){
+    return next;
+  }
+
+  public String toString(){
+    return this.data.toString();
+  }
+}
