523186057597cfb598fc7636561727c2d30f6c91
Alsnj20 <marieljara656@gmail.com>
2024-05-24 13:52:07 -0500

    Clase PilaArray

--- Pilas/Pila.java
+++ Pilas/Pila.java
@@ -1,54 +1,63 @@
 package Pilas;
+
 import List.*;
+
 //LIFO
-public class Pila<T> implements Stack<T>{
+public class Pila<T>implements Stack<T> {
 
   private int tope;
   private int size;
   private T[] pilaArray;
 
-  public Pila(int size){
+  public Pila(int size) {
     this.size = size;
     this.pilaArray = (T[]) new Object[size];
-    this.tope = -1;
+    this.tope = 0;
   }
 
   public void push(T data){
-    if(!isFull()){
-      /*tope++;
-      pilaArray[tope] = data;*/
-      pilaArray[++tope] = data;
-    }else{
-      T[] pilaArray2 = (T[]) new Object[this.size*2];
-      this.pilaArray = pilaArray2;    
+    if (!isFull()) {
+      this.pilaArray[tope] = data;
+      this.tope++;
+    } else {
+      T[] pilaArray2 = (T[]) new Object[this.size * 2];
+      this.pilaArray = pilaArray2;
     }
   }
 
-  public T pop(){
+  public T pop() {
+    if (!isEmpty()) {
+      T data = this.pilaArray[this.tope];
+      this.pilaArray[this.tope] = null;
+      this.tope--;
+      return data;
+    }
     return null;
   }
-  public T top(){
-    if(isEmpty())
-    return this.pilaArray[this.tope];
-  }
-  public boolean isEmpty(){
-    return this.tope == -1;
+
+  public T top() {
+    if (isEmpty()) {
+      return (T) this.pilaArray[this.tope];
+    }
+    return null;
   }
 
-  public boolean isFull(){
-    return this.tope == this.pilaArray.length-1;
+  public boolean isEmpty() {
+    return this.tope == 0;
   }
 
-  public String toString(){
+  public boolean isFull() {
+    return this.tope == this.pilaArray.length - 1;
+  }
+
+  public String toString() {
     String str = "";
     for (int i = this.tope; i >= 0; i--) {
-      if(this.pilaArray[i] != null){
+      if (this.pilaArray[i] != null) {
         str += this.pilaArray[i] + "\n";
       }
     }
     return str;
   }
 
-
-  
 }
---
+++ Pilas/Test.java
@@ -0,0 +1,17 @@
+package Pilas;
+
+public class Test {
+  public static void main(String[] args) {
+    Stack<Character> st = new Pila<>(0);
+    try{
+      st.push('a');
+      st.push('b');
+      st.push('c');
+      System.out.println(st.toString());
+      System.out.println(st.top());
+      System.out.println(st.toString());
+    }catch(Exception e){
+      System.out.println(e.getMessage());
+  }
+}
+}
