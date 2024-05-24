2ca4fc940363108e1287054b141ec04b726a9c23
Alsnj20 <marieljara656@gmail.com>
2024-05-24 13:27:49 -0500

    Clase Pila

---
+++ Pilas/Pila.java
@@ -0,0 +1,38 @@
+package Pilas;
+import List.*;
+
+public class Pila<T> implements Stack<T>{
+
+  private int tope = -1;
+  private int size;
+  private T[] pila;
+
+  public Pila(int size){
+    this.size = size;
+    pila = (T[]) new Object[size];
+  }
+
+  public void push(T data){
+    if(tope < size-1){
+      tope++;
+      pila[tope] = data;
+    }
+  }
+
+  public T pop(){
+    return null;
+  }
+  public T top(){
+    return null;
+  }
+  public boolean isEmpty(){
+    return true;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+
+  
+}
