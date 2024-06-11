112d5be004e070cea835c2db86877718f581365c
Alsnj20 <marieljara656@gmail.com>
2024-06-11 11:59:02 -0500

    Implements Dictionary Interface

---
+++ SegundaUnidad/Arboles/DICT/Dictionary.java
@@ -0,0 +1,11 @@
+package DICT;
+
+import BST.ItemDuplicated;
+import BST.ItemNotFound;
+
+public interface Dictionary<K,V> {
+  void insert(K key, V value) throws ItemDuplicated;
+  V search(K key) throws ItemNotFound;
+  void remove(K key) throws ItemNotFound;
+  boolean isEmpty();
+}
